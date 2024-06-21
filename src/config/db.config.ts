e7b2d895a2a9c6df3b141e72f25e7bc881255fc3
Savlms <valmarnas@gmail.com>
2024-06-20 23:33:05 -0700

    post model

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,10 +4,10 @@ mongoose.set('strictQuery', true);
 export default function database() {
     mongoose.connect(process.env.DB_URI!)
         .then(() => {
-            console.log("MongoDB is connected")
+            console.log("MongoDB is connected");
         })
-        .catch(() =>  {
+        .catch((err) =>  {
             console.log (
-                "There is an error connecting to the database")
+                "There was an error connecting to the database");
         });
 }
